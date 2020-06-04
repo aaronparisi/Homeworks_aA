9f2528fa8b4724fbd3680ad7b8e2dc244edca458
Aaron <parisi.aaron@gmail.com>
2020-06-03 17:05:46 -0700

    poly tree search algos

--- PolyTree/skeleton/lib/00_tree_node.rb
+++ PolyTree/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
 
     attr_writer :children
@@ -12,19 +14,47 @@ class PolyTreeNode
 
     def parent=(node)
         # remove self from self.parent's children, if it's in there
-        self.parent.remove(self) if parent
+        self.parent.remove_child(self) if parent && node != nil
         @parent = node
         node.children.push(self) if node && ! node.children.include?(self)
     end
 
-    def remove(child)
+    def add_child(child)
+        @children << child
+        child.parent = self
+    end
+
+    def remove_child(child)
         @children.delete_at(children.index(child))
+        child.parent = nil
+    end
+
+    def dfs(tgt)
+        return self if value == tgt
+        children.each do |child|
+            child_search = child.dfs(tgt)
+            return child_search if child_search
+        end
+        nil
+    end
+
+    def bfs(tgt)
+        #return self is value == tgt
+        nodes = []
+        nodes << self
+        until nodes.empty?
+            cur_node = nodes.shift
+            return cur_node if cur_node.value == tgt
+            cur_node.children.each {|child| nodes << child}
+            #nodes += cur_node.children
+        end
+        nil
     end
 
 
-    def inspect()
-        #"parent" => parent.value; "value" => value; => "children" => children.map {|child| child.value}}
-        #children = [#{children.map {|child| child.value}.join(",")}]
-        "[parent => #{parent ? parent.value : "none"}; value => #{value}]"
-    end
+    # def inspect()
+    #     #"parent" => parent.value; "value" => value; => "children" => children.map {|child| child.value}}
+    #     #children = [#{children.map {|child| child.value}.join(",")}]
+    #     "[parent => #{parent ? parent.value : "none"}; value => #{value}]"
+    # end
 end
