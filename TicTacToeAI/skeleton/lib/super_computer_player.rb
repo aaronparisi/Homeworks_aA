925e86ad303782983474e0312c9df3473ec8d579
Aaron <parisi.aaron@gmail.com>
2020-06-06 11:24:26 -0700

    created super computer player

--- TicTacToeAI/skeleton/lib/super_computer_player.rb
+++ TicTacToeAI/skeleton/lib/super_computer_player.rb
@@ -2,6 +2,13 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    # the computer player just guesses a winning guess, if there is one,
+    # or a random one... not super smart
+    anode = TicTacToeNode.new(game.board, mark)
+    kiddos = anode.children
+    kiddos.each {|akid| return akid.prev_move_pos if akid.winning_node?(mark)}
+
+    kiddos.filter {|akid| ! akid.losing_node?(mark)}.sample.prev_move_pos
   end
 end
 
