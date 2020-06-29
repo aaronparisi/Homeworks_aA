da1c381bc2d6e9972884903a3be63e96286975fd
Aaron <parisi.aaron@gmail.com>
2020-06-29 13:58:24 -0700

    LRU Cache building - max int & int set

---
+++ Ruby/LRU_Cache/skeleton/lib/p01_int_set.rb
@@ -0,0 +1,97 @@
+require 'byebug'
+
+class MaxIntSet
+
+  attr_reader :max, :store
+
+  def initialize(max)
+    @max = max
+    @store = Array.new(max)
+  end
+
+  def insert(num)
+    raise "Out of bounds" if ! is_valid?(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num <= @max && num >= 0
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+
+  attr_reader :store
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num if ! @store.include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
