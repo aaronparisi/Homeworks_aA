67508766253c93c1774941d276f2a8a67c1a5db3
Aaron <parisi.aaron@gmail.com>
2020-06-29 14:52:52 -0700

    array and string hash fns

--- Ruby/LRU_Cache/skeleton/lib/p02_hashing.rb
+++ Ruby/LRU_Cache/skeleton/lib/p02_hashing.rb
@@ -4,11 +4,20 @@ end
 
 class Array
   def hash
+    hc = self.length
+    self.each do |el|
+      hc *= 31
+      hc += el.hash  # our array can have int, string, or hash elements
+    end
+
+    hc
   end
 end
 
 class String
   def hash
+    # use Array#hash
+    self.chars.map(&:ord).hash # we hash over an array of ASCII vals
   end
 end
 
