e4d4cd987c0a9ddaac7461a87b9b7f35c428de8d
Aaron <parisi.aaron@gmail.com>
2020-06-29 18:31:12 -0700

    hashing resizing set

--- Ruby/LRU_Cache/FullProj/skeleton/lib/p03_hash_set.rb
+++ Ruby/LRU_Cache/FullProj/skeleton/lib/p03_hash_set.rb
@@ -7,18 +7,27 @@ class HashSet
   end
 
   def insert(key)
+    return if include?(key)
+
+    resize! if @count >= num_buckets
+
+    self[key] << key
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    @count = [@count - 1, 0].max if self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +35,12 @@ class HashSet
   end
 
   def resize!
+    temp = Array.new(num_buckets*2) {Array.new}
+
+    @store.each do |el|
+      temp[el.hash % (num_buckets*2)] << el
+    end
+
+    @store = temp
   end
 end
