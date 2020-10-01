4ac1cd33cea90c76e87b61aa54e38a02cc6ded8f
Aaron <parisi.aaron@gmail.com>
2020-10-01 14:30:06 -0700

    advanced queries pt 2 and 3

--- Rails/AdvancedQueries/AdvancedQueries_MovieBuff/movie_buff/02_queries.rb
+++ Rails/AdvancedQueries/AdvancedQueries_MovieBuff/movie_buff/02_queries.rb
@@ -3,17 +3,42 @@ def eighties_b_movies
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
 
+  Movie.where('yr between ? and ?', 1980, 1989)
+    .where('score between ? and ?', 3, 5)
+    .select(:id, :score, :title, :yr)
+
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
 
+  goodYears = Movie.where('score >= 8').select(:yr)
+
+  # Movie.where.not('yr in goodYears')
+  #   .select(:yr)
+  #   .uniq
+  # I'm not sure how to turn the array of goodYears into a list
+
+  Movie.group(:yr).having('MAX(score) < 8').pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
 
+  # Movie.where(title: title)
+  #   .joins(:actors)
+  #   .order('castings.ord')
+  #   .select('actors.id, actors.name')
+  # this WORKS, but notice that we are ultimately looking for ACTOR info
+
+  Actor.joins(:movies)
+    .where(movies: { title: title })
+    .order('castings.ord')
+    #.select(actors: { :id, :name })
+    .select('actors.id, actors.name')
+  # here we have to be specific about which table :title comes from
+  # whereas above, we put the where clause right on movies prior to the join
 end
 
 def vanity_projects
@@ -23,10 +48,23 @@ def vanity_projects
 
   # Note: Directors appear in the 'actors' table.
 
+  Movie.joins(:actors)
+    .where('actors.id = movies.director_id')
+    .where('castings.ord = 1')
+    .select('movies.id, movies.title, actors.name')
+
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
 
+  Actor.joins(:castings)
+    # .where('castings.ord <> 1')
+    .where.not(castings: { ord: 1 })
+    .group('actors.id')
+    # .order('count(actors.id) desc')
+    .order('roles DESC')
+    .select('actors.id, actors.name, count(actors.id) as roles')
+    .limit(2)
 end
--- Rails/AdvancedQueries/AdvancedQueries_MovieBuff/movie_buff/03_queries.rb
+++ Rails/AdvancedQueries/AdvancedQueries_MovieBuff/movie_buff/03_queries.rb
@@ -2,11 +2,46 @@ def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
 
+  # Movie.joins(:actors)
+  # .where(actors: { name: those_actors })
+  # .where(castings: { ord: 1 })
+  #   .select('movies.title, movies.id')
+  # this shows "movies whose starring actor is one of the names in the list"
+  # I think they want "movies where EACH actor in the list is in the cast"
+
+  Movie.select(:title, :id)
+    .joins(:actors)
+    .where(actors: { name: those_actors })
+    .group(:id)
+    .having('count(movies.id) >= ?', those_actors.length)
+
 end
 
 def golden_age
   # Find the decade with the highest average movie score.
 
+  # Movie.select('avg(score) as avg_score')
+  #   .group(:yr)
+  #   .having('yr like ?', )
+  #   .order('AVG(movies.score)')
+  #   .limit(1)
+
+  Movie.select('(yr / 10) * 10 as decade') # integer division rounds
+    .group('decade') # decade is a SQL thing, not an active record thing
+    .order('avg(score) desc')
+    # .limit(1) # this returns an ActiveRecordRelation
+    .first
+    .decade
+
+    # lessons:
+    # 1. we have to CONSTRUCT decades, and we can refer to it as a SQL thing
+    # 2. limit returns an active record object, first returns an ARRAY
+    # if we leave out .first, we get all the decades
+    # if we leave off .decade, we get a MOVIE (NOT an ActiveRecord object)
+
+    # limit:
+    # expected #<Integer:3841> => 1920
+    # got #<Movie::ActiveRecord_Relation:12940> => #<ActiveRecord::Relation [#<Movie id: nil>]>
 end
 
 def costars(name)
@@ -14,10 +49,30 @@ def costars(name)
   # appeared with.
   # Hint: use a subquery
 
+  theirMovies = Movie.select(:title).joins(:actors).where(actors: { name: name })
+
+  Actor
+    .joins(:movies)
+    .where(movies: { title: theirMovies })
+    .where.not(actors: { name: name })
+    .uniq
+    .pluck(:name)
+
 end
 
 def actor_out_of_work
   # Find the number of actors in the database who have not appeared in a movie
+
+  # withCastings = Actor.joins(:castings).uniq.pluck(:name)
+
+  # Actor.where.not(name: withCastings).length
+  # ^^ this makes TWO queries, no bueno
+
+  Actor.joins("LEFT JOIN castings on actors.id = castings.actor_id")
+    .where('castings.id is null')
+    .count
+  # we have to be specific about which kind of join we want to do
+  # the default join is INNER JOIN
 
 end
 
@@ -29,6 +84,13 @@ def starring(whazzername)
   # ex. "Sylvester Stallone" is like "sylvester" and "lester stone" but
   # not like "stallone sylvester" or "zylvester ztallone"
 
+  # ins = whazzername.split(' ').join('').split('').join('%')
+  # this WORKS but it's not ideal
+  ins = "%#{whazzername.split(//).join('%')}%"
+
+  Movie.joins(:actors)
+    .where('lower(actors.name) like ?', ins)
+
 end
 
 def longest_career
@@ -36,5 +98,12 @@ def longest_career
   # (the greatest time between first and last movie).
   # Order by actor names. Show each actor's id, name, and the length of
   # their career.
+
+  Actor.joins(:movies)
+    .select(:id, :name, ('(max(movies.yr)-min(movies.yr)) as career'))
+    .group('actors.id, actors.name')  # unnecessary; we are not aggregating career
+    .order('career desc')
+    .limit(3)
+
 
 end
