809146da79976c1ae0276299d38b8c82f04be114
Aaron <parisi.aaron@gmail.com>
2020-10-06 19:35:44 -0700

    my active record table name, column names getters and setters

--- Rails/MyActiveRecord/skeleton/lib/01_sql_object.rb
+++ Rails/MyActiveRecord/skeleton/lib/01_sql_object.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 require_relative 'db_connection'
 require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
@@ -5,18 +6,30 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    # ...
+    # returns an array of the corresponding database table's column names
+    unless @columns
+      query = DBConnection.execute2(<<-SQL)
+        select *
+        from "#{self.table_name}"
+      SQL
+      @columns = query.first.map(&:to_sym)
+    end
+
+    return @columns
   end
 
   def self.finalize!
   end
 
   def self.table_name=(table_name)
-    # ...
+    # sets the table name attribute for this class
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    # returns the name of the database table for the class, like
+    # User.table_name => users
+    @table_name ? @table_name : self.to_s.tableize
   end
 
   def self.all
