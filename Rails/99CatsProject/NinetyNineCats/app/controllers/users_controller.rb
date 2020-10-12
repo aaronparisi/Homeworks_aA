4d07b385a64f4d1b44f1f587cdfea44cb600347d
Aaron <parisi.aaron@gmail.com>
2020-10-12 13:43:27 -0700

    user class, views

---
+++ Rails/99CatsProject/NinetyNineCats/app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.save(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to users_path, notice: "User successfully destroyed"
+    else
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+  
+  
+end
---
+++ Rails/99CatsProject/NinetyNineCats/app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+  validates :name, :email, presence: :true
+  validates :email, uniqueness: true
+end
